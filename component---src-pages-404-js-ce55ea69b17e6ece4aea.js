b6d0ea0e394b99b56e5d95d5989a413d13d9f45f
AX99 <rapha.mensah@gmail.com>
2022-04-19 07:32:28 +0100

    Updates

---
+++ component---src-pages-404-js-ce55ea69b17e6ece4aea.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_landing_013_gatsby=self.webpackChunk_landing_013_gatsby||[]).push([[883],{429:function(e,t,n){n.r(t);var l=n(7294),a=n(533),u=n(262);t.default=function(){return l.createElement(a.Z,null,l.createElement(u.Z,{title:"404: Not found"}),l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ce55ea69b17e6ece4aea.js.map
