04add24c92236fbce66e279fd3e3a2d73615462c
AX99 <rapha.mensah@gmail.com>
2022-04-19 07:38:54 +0100

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,10 +26,12 @@ export default function IndexPage() {
         overlay={true}
       >
         We use cookies on this website to enhance your user experience. By
-        continuing on this website you are agreeing to the use of these cookies.{" "} 
-        
-        <Link path="https://www.privacypolicies.com/live/2419c67c-502a-4c72-a39c-29efa479a217">
-         Click to view our Cookies Policy
+        continuing on this website you are agreeing to the use of these cookies.{" "}
+        <Link
+          path="https://www.privacypolicies.com/live/2419c67c-502a-4c72-a39c-29efa479a217"
+          target="_blank"
+        >
+          Click to view our Cookies Policy
         </Link>
         .
       </CookieConsent>
